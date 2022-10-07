671533039e1869c43f7c49231564626299c50332
ShrikantBhadke <111225704+ShrikantBhadke@users.noreply.github.com>
2022-10-07 20:20:37 +0530

    Change and Submit Event update

---
+++ 29 DOM Change and Submit Event/assets/js/main.js
@@ -0,0 +1,25 @@
+const submitForm = document.getElementById("submitForm");
+const fnameInput = document.getElementById("fname");
+const education = document.getElementById("education");
+const emailInput = document.getElementById("email");
+const paraPrint = document.getElementById("para");
+let fname;
+let education1;
+let email;
+
+const subFormData = (event)=>{
+    event.preventDefault();
+    fname = fnameInput.value;
+    education1 = education.value;
+    email = emailInput.value;
+    console.log(fname);
+    console.log(education1);
+    console.log(email);
+
+    paraPrint.innerHTML = `Full Name :- ${fname} <br> Education:- ${education1} <br> E-mail Id:- ${email}`
+// console.log(event.target);
+}
+//
+
+
+submitForm.addEventListener("submit",subFormData);
