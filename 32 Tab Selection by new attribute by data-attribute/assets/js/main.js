c9e6d718db05d1018ee662761adde2c3d18fa073
ShrikantBhadke <111225704+ShrikantBhadke@users.noreply.github.com>
2022-10-08 12:23:07 +0530

    diff way to do tab using data-id atribute

--- 32 Tab Selection by new attribute by data-attribute/assets/js/main.js
+++ 32 Tab Selection by new attribute by data-attribute/assets/js/main.js
@@ -1,56 +1,91 @@
 const liEle = Array.from(document.querySelectorAll(".tapHeading li"));
 const paraEle = Array.from(document.querySelectorAll(".contPara p"));
+// ==========================================================================================
+// const selectedTab = (event) =>{
+//   console.log(event.target);
+//   liEle.forEach((li)=>{
+//     li.classList.remove("active");
+// })
+//   event.target.classList.add("active")
+// paraEle.forEach((para)=>{
+//   para.style.display = "none";
+// })
+//  document.getElementById(event.target.dataset.id).style.display = "block";
+// }
 
+// liEle.forEach((li)=>{
+//   li.addEventListener("click",selectedTab)
+// })
+// ==========================================================================================
 
-const selectTab =(event) =>{
-  let liselect = event.target.dataset
-    console.log(liselect);
+const selectedTab = (event) => {
+  // console.log(event.target);
+  liEle.forEach((li) => li.classList.remove("active"));
+  event.target.classList.add("active");
+  paraEle.forEach((para) => (para.style.display = "none"));
+  document.getElementById(event.target.dataset.id).style.display = "block";
+};
 
-  paraEle.forEach((para)=>{
-    para.classList.remove("activePara")
-    console.log(para)
-  })
-
-  
-}
-
-
-liEle.forEach((li)=>{
-  li.addEventListener("click",selectTab)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
+liEle.forEach((li) => li.addEventListener("click", selectedTab));
+// ==========================================================================================
 
 
 
 
 
+// const selectedli = (getli) =>{
+//   liEle.forEach((li)=>{
+//     li.classList.remove("active")
+//   })
+//   getli.classList.add("active");
+// }
+// const selectpara = (getli) =>{
+//   paraEle.forEach((para)=>{
+//     para.classList.remove("activePara");
+//   })
+//   let selectedtabpara = document.getElementById(getli.dataset.id);
+//   selectedtabpara.classList.add("activePara")
+// }
+
+// const selectTab = (event) =>{
+//   // let liEle = event.target;
+//   // console.log(liEle);
+//   selectedli(event.target)
+//   selectpara(event.target)
+
+// }
+
+// liEle.forEach((li)=>{
+//   li.addEventListener("click", selectTab)
+// })
+// ==========================================================================================
 
 
 
 
 
+// const selectTab =(event) =>{
+//   let liselect = event.target
+//     console.log(liselect);
 
+//   paraEle.forEach((para)=>{
+//     para.classList.remove("activePara")
+//     // console.log(para)
+//   })
+//   let selectedli = document.getElementById(event.target.dataset.id)
+//   // console.log(selectedli)
+//   selectedli.classList.add("activePara");
+//   liEle.forEach((li)=>{
+//     li.classList.remove("active")
+//   })
+//   liselect.classList.add("active")
 
+// }
 
-
-
-
-
-
-
-
+// liEle.forEach((li)=>{
+//   li.addEventListener("click",selectTab)
+// })
+// ==========================================================================================
 
 
 
@@ -62,7 +97,7 @@ liEle.forEach((li)=>{
 //     liEle.forEach(function(ele){
 //       ele.classList.remove("active");
 //     })
-//     this.classList.add("active"); 
+//     this.classList.add("active");
 //     let result = document.querySelector(tabLi);
 //     console.log(result);
 //     paraEle.forEach((para)=>{
@@ -71,6 +106,9 @@ liEle.forEach((li)=>{
 //     result.classList.add("activePara")
 //   })
 // })
+
+
+// ==========================================================================================
 
 
 
@@ -82,12 +120,12 @@ liEle.forEach((li)=>{
 //       selectpara(this)
 //     })
 //   })
-  
+
 //   function selectLi(li){
 //     liEle.forEach(function(ele){
 //       ele.classList.remove("active");
 //     })
-//     li.classList.add("active"); 
+//     li.classList.add("active");
 //   }
 //   function selectpara(para){
 //     let tabLi = '#' + para.innerText;
