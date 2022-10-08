fcc5d1d21ff4c6bf22c9f4e03f1e0e639dfea121
ShrikantBhadke <111225704+ShrikantBhadke@users.noreply.github.com>
2022-10-08 07:40:40 +0530

    selection of li and para in javascript

---
+++ 31 Tab Selection/assets/js/main.js
@@ -0,0 +1,47 @@
+let liEle = Array.from(document.querySelectorAll(".tapHeading li"));
+let paraEle = Array.from(document.querySelectorAll(".contPara p"));
+
+
+// liEle.forEach((ele)=>{
+//   ele.addEventListener("click",function(onclick){
+//     // console.log(onclick.target.innerText)
+//     let tabLi = '#' + onclick.target.innerText;
+//     liEle.forEach(function(ele){
+//       ele.classList.remove("active");
+//     })
+//     this.classList.add("active"); 
+//     let result = document.querySelector(tabLi);
+//     console.log(result);
+//     paraEle.forEach((para)=>{
+//         para.classList.remove("activePara");
+//     })
+//     result.classList.add("activePara")
+//   })
+// })
+
+
+
+liEle.forEach((ele)=>{
+    ele.addEventListener("click",function(onclick){
+      console.log(this.innerText)
+
+      selectLi(this)
+      selectpara(this)
+    })
+  })
+  
+  function selectLi(li){
+    liEle.forEach(function(ele){
+      ele.classList.remove("active");
+    })
+    li.classList.add("active"); 
+  }
+  function selectpara(para){
+    let tabLi = '#' + para.innerText;
+    let result = document.querySelector(tabLi);
+      console.log(result);
+      paraEle.forEach((para1)=>{
+          para1.classList.remove("activePara");
+      })
+      result.classList.add("activePara")
+  }
