37afa1e6b2a72de3f813cfead0c8cc532b4f6060
ShrikantBhadke <111225704+ShrikantBhadke@users.noreply.github.com>
2022-10-09 11:49:49 +0530

    find findIndex update

--- 33 find findIndex reduce() HOF/assets/js/main.js
+++ 33 find findIndex reduce() HOF/assets/js/main.js
@@ -1,18 +1,69 @@
-let ages = [12,89,45,79,56,78,42,53,15,78,15];
+let ages = [12, 89, 45, 79, 56, 78, 42, 53, 15, 78, 15];
 
 let companies = [
-  { name: "Company One",id:1, category: "Finance", start: 1981, end: 2003 },
-  { name: "Company Two",id:2, category: "Retail", start: 1992, end: 2008 },
-  { name: "Company Three",id:3, category: "Auto", start: 1999, end: 2007 },
-  { name: "Company Four",id:4, category: "Retail", start: 1989, end: 2010 },
-  { name: "Company Five",id:5, category: "Technology", start: 2009, end: 2014 },
-  { name: "Company Six",id:6, category: "Finance", start: 1987, end: 2010 },
-  { name: "Company Seven",id:7, category: "Auto", start: 1986, end: 1996 },
-  { name: "Company Eight",id:8, category: "Technology", start: 2011, end: 2016 },
-  { name: "Company Nine",id: 9 , category: "Retail", start: 1981, end: 1989 }
+  { name: "Company One", id: 1, category: "Finance", start: 1981, end: 2003 },
+  { name: "Company Two", id: 2, category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", id: 3, category: "Auto", start: 1999, end: 2007 },
+  { name: "Company Four", id: 4, category: "Retail", start: 1989, end: 2010 },
+  {
+    name: "Company Five",
+    id: 5,
+    category: "Technology",
+    start: 2009,
+    end: 2014,
+  },
+  { name: "Company Six", id: 6, category: "Finance", start: 1987, end: 2010 },
+  { name: "Company Seven", id: 7, category: "Auto", start: 1986, end: 1996 },
+  {
+    name: "Company Eight",
+    id: 8,
+    category: "Technology",
+    start: 2011,
+    end: 2016,
+  },
+  { name: "Company Nine", id: 9, category: "Retail", start: 1981, end: 1989 },
 ];
+
+let idfindindex = 3;
+
+let findIndexId3 = companies.findIndex((comp)=>comp.id === idfindindex);
+console.log(findIndexId3);
+
+
+
+let financedur10 = companies
+  .filter(
+    (comp) =>
+      comp.category.toLowerCase().trim() === "finance" &&
+      comp.end - comp.start >= 10
+  )
+  .map((comp) => ({
+    companyName: comp.name,
+    catagory: comp.category,
+    duration: comp.end - comp.start,
+  }));
+console.log(financedur10);
+let retailObj = companies
+  .filter((comp) => comp.category.toLowerCase().trim() === "retail")
+  .map((comp) => ({
+    companyName: comp.name,
+    duration: comp.end - comp.start,
+    catagory: comp.category,
+  }));
+
+console.log(retailObj);
+
+let financeObjs = companies
+  .filter((comp) => comp.category.toLowerCase().trim() === "finance")
+  .map((comp) => ({
+    companyName: comp.name,
+    duration: comp.end - comp.start,
+    catagory: comp.category,
+  }));
+
+console.log(financeObjs);
 let findId = 4;
-let compId = companies.find((comp)=>comp.id === findId);
+let compId = companies.find((comp) => comp.id === findId);
 console.log(compId);
 
 // let findId9 = 9;
@@ -20,15 +71,21 @@ console.log(compId);
 // console.log(compId9)
 
 let findId9 = "9";
-let compId9 = companies.find((comp)=> comp.id === +findId9)
-console.log(compId9)
-let findIndex5 = 5
-let indexid5 = companies.findIndex((comp)=> comp.id === findIndex5)
+let compId9 = companies.find((comp) => comp.id === +findId9);
+console.log(compId9);
+let findIndex5 = 5;
+let indexid5 = companies.findIndex((comp) => comp.id === findIndex5);
 console.log(indexid5);
 
-
-let sumAge = ages.reduce((preVal,currVal)=>(preVal+=currVal),0)
+let sumAge = ages.reduce((preVal, currVal) => (preVal += currVal), 0);
 console.log(sumAge);
 
-let Multge = ages.reduce((preVal,currVal)=>(preVal*=currVal),1)
+let Multge = ages.reduce((preVal, currVal) => (preVal *= currVal), 1);
 console.log(Multge);
+
+
+ages = [12, 89, 45, 79, 56, 78, 42, 53, 15, 78, 15];
+
+
+let subStrctages = ages.reduce((preVal,currVal)=>(preVal -= currVal),0);
+console.log(subStrctages)
